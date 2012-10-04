3a6d96317d4e6b0e1186a9b5d9fcc00d66ccb93f
Lam Nguyen <me@Lam-Nguyens-iMac.local>
2012-10-04 16:17:35 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weddability::Application.config.secret_token = '4b41ed230badfc77b17a854af293db5ab41857d824f3019b41c8b68a01e7d48ec36896559c1185bd1947020f48ea655f3aa3478aae75ace4bb0d2d6aed01ffe2'
