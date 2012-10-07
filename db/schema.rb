9203669e1c513d15985931bfc791b1ab6cdfa4fa
Lam Nguyen <me@madebylam.com>
2012-10-07 05:49:04 -0500

    messed up with address model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006120244) do
+ActiveRecord::Schema.define(:version => 20121007073249) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -32,6 +32,23 @@ ActiveRecord::Schema.define(:version => 20121006120244) do
 
   add_index "accounts", ["email"], :name => "index_wedd_accounts_on_email", :unique => true
   add_index "accounts", ["reset_password_token"], :name => "index_wedd_accounts_on_reset_password_token", :unique => true
+
+  create_table "addresses", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company"
+    t.string   "street_address"
+    t.string   "extended_address"
+    t.string   "city"
+    t.string   "zip"
+    t.string   "country"
+    t.boolean  "default"
+    t.integer  "account_id"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
+
+  add_index "addresses", ["account_id"], :name => "index_wedd_addresses_on_account_id"
 
   create_table "pages", :force => true do |t|
     t.string   "name"
