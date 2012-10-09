fb60d007dc4c0114403806aacabb31cb9ed6e1dc
Lam Nguyen <me@madebylam.com>
2012-10-09 03:15:54 -0500

    add credit card masking method

--- app/models/order_transaction.rb
+++ app/models/order_transaction.rb
@@ -4,9 +4,18 @@ class OrderTransaction < ActiveRecord::Base
 
   serialize :result
 
-  def response=(response)
-  	self.success = response.success?
-  	self.message = response.message
-  end
+	def response=(response)
+		self.success = response.success?
+		self.message = response.message
+	end
+
+
+	def last_digits(number)    
+	 	number.to_s.length <= 4 ? number : number.to_s.slice(-4..-1) 
+	end
+
+	def mask(number)
+		"XXXX-XXXX-XXXX-#{last_digits(number)}"
+	end
 
 end
